67fa983b1b5f2324770d0c3e3bb3eb2dd14b19d8
angelmt2396 <al045219@uacam.mx>
2021-09-09 19:33:05 -0500

    no se :v

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Login from './components/auth/Login';
-import Signup from './components/auth/Signup';
-=======
+import React from "react";
 import AuthProvider from "./Components/auth/AuthProvider";
 import AppRouter from "./Routers/AppRouter";
 require('dotenv').config();
->>>>>>> development
 
 function App() {
   return (
