e5c04988396d11030a65893ca082163fe0a21f1c
angelmt2396 <al045219@uacam.mx>
2021-09-15 19:54:06 -0500

    :v

--- src/Pages/AdminPage.js
+++ src/Pages/AdminPage.js
@@ -1,43 +1,13 @@
 import AgregarPregunta from '../Preguntas/AgregarPregunta';
 import ReactDOM from 'react-dom';
 import { NavLink } from 'react-router-dom';
-import NetworkManager from "../Backend/util/http";
-import { useEffect } from 'react';
 
 export default function AdminPage() {
-  var categories;
-  var secciones = [];
-
-  var net = new NetworkManager();
-
-  const recibirCategorias = async () => {
-    var response = await net.globalGet('/admin/categorias'); 
-    console.log(response);
-    var rawData = await response.data.data;
-
-    categories = rawData;
-
-    //id categoria es el nombre de la seccion
-    //contiene categorias
-
-        for(var i in rawData){
-            await secciones.push(
-                <option value={i} key={rawData[i].id_categoria} name={rawData[i].id_categoria}
-                >{rawData[i].id_categoria}</option>
-            );
-        }
-        //ReactDOM.render(secciones, document.getElementById('selectSecciones'));
-    }
-
-    useEffect ( () => {
-      // El useEffect recibe un parametro vacio o con alguna dependencia (como por ejemplo ajustar valores de retorno)
-      // Si no se tiene un parametro indicador al final ([]) esto refrescara el estado y rerenderizara la pantalla. pendejo.
-      recibirCategorias();
-  }, []);
+  
   const handleClick = (e) => {
-    if(e.target.value==="agregar_pregunta"){  
+    if(e.target.value ==="agregar_pregunta"){  
       ReactDOM.render(
-        <AgregarPregunta  secciones={secciones} categories={categories}/>,
+        <AgregarPregunta />,
         document.getElementById('adminContainer')
      ); 
      
