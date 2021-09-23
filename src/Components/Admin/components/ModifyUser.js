6890d66a2d080cc7b050bddc7e7e8dd72dee000c
Jorge E. Espadas <al050610@uacam.mx>
2021-09-22 21:20:37 -0500

    little changes

--- src/Components/Admin/components/ModifyUser.js
+++ src/Components/Admin/components/ModifyUser.js
@@ -1,20 +1,9 @@
-import React, {useContext, useState} from 'react';
+import React, {useState} from 'react';
 import { Spinner } from 'react-bootstrap';
 import NetworkManager from '../../../Backend/util/Http';
 import Util from '../../../Backend/util/Util';
-import { AuthContext } from '../../auth/AuthProvider';
 
 function ModifyUser({snackbar}) {
-    /**
-     * En esta pantalla necesito mostrar una barra de busqueda con un boton
-     * Dicha barra recibe el email del target y al buscar debo mostrar cualquier tipo de loadingComponent
-     * Cuando los datos sean obtenidos, pintar de manera dinamica el mismo (o parecido) formulario de userCreate.
-     * Con la modificacion de que algunos valores no estaran para editar como el nombre, email, telefono.
-     * Las contrasenas se pueden cambiar, al igual que el rol.
-     * 
-     * Implementar encripcion de lado del front-end (ya sea algo sencillo como MD5, o SHA1)
-     * 
-     */
     const [search, setSearch] = useState('');
     const [loading, setLoading] = useState(false);
     const [submitLoading, setSubmit] = useState(false);
