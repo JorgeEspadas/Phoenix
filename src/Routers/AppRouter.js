8807fdd9e43c2c6d7e8bf975fc9af2a55fbccb44
Jorge <al050610@uacam.mx>
2021-10-10 18:39:14 -0500

    quitados los errores

--- src/Routers/AppRouter.js
+++ src/Routers/AppRouter.js
@@ -13,7 +13,6 @@ import PrivateRoute from "./PrivateRoute";
 import PublicRoute from "./PublicRoute";
 import HomePage from "../Pages/HomePage";
 import AnaliticsPage from "../Pages/AnaliticsPage";
-import InfoPage from "../Pages/InfoPage";
 import ContactPage from "../Pages/ContactPage";
 import FormPage from "../Pages/FormPage";
 import AdminPage from "../Pages/AdminPage";
@@ -24,15 +23,14 @@ import Login from "../Pages/Login.js";
 export default function AppRouter() {
   return (
     <Router>
-      <div class="container-fluid overflow-hidden">
-        <div class="row vh-100 overflow-auto">
+      <div className="container-fluid overflow-hidden">
+        <div className="row vh-100 overflow-auto">
           <Sidebar />
-          <div class="col d-flex flex-column h-sm-100">
+          <div className="col d-flex flex-column h-sm-100">
             <main className="row overflow-auto">
               <Switch>
                 <Route exact path="/" component={HomePage} />
                 <Route exact path="/Estadisticas" component={AnaliticsPage} />
-                <Route exact path="/Info" component={Login} />
                 <Route exact path="/Contactanos" component={ContactPage} />
                 <Route exact path="/Login" component={Login}/>
 
@@ -50,8 +48,8 @@ export default function AppRouter() {
                 <Route exact path="*" component={NotFoundPage} />
               </Switch>
             </main>
-            <footer class=" footer row py-4 mt-auto">
-              <div class="col"> JAM | 2021 </div>
+            <footer className=" footer row py-4 mt-auto">
+              <div className="col"> JAM | 2021 </div>
             </footer>
           </div>
         </div>
