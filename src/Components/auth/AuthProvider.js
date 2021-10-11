59eb78f980feeb146f190a7f8bdd066023130973
Jorge <al050610@uacam.mx>
2021-10-11 00:49:17 -0500

    rip mocos

--- src/Components/auth/AuthProvider.js
+++ src/Components/auth/AuthProvider.js
@@ -2,7 +2,6 @@ import { createContext, useState, useEffect } from "react";
 
 export const AuthContext = createContext();
 
-
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(
     JSON.parse(localStorage.getItem("user")) || null
