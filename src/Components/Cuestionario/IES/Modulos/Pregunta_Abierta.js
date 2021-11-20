30fddafa17e886fbe76e89950e9fcbe85aad89d5
Jorge <al050610@uacam.mx>
2021-11-20 14:53:14 -0600

    in-dev: design done, testing functions

--- src/Components/Cuestionario/IES/Modulos/Pregunta_Abierta.js
+++ src/Components/Cuestionario/IES/Modulos/Pregunta_Abierta.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { Col, Form, Row } from "react-bootstrap";
+
+const handleChange = e => {
+    // filter response, and send it back to the source through the callback
+}
 
 function PreguntaAbierta({modulo, callback}){
-
     return (
-        <Form.Group as={Row} className="mb-3">
-            <Form.Label column sm="7">{modulo.texto}</Form.Label>
-            <Col sm="5">
-            <Form.Control onChange={callback} name={modulo.id} type="text"></Form.Control>
-            </Col>
-        </Form.Group>
+        <div className="container border-bottom px-1 p-2">
+            <div className="row p-1">
+                <div className="col-sm-7 px-1">
+                    {modulo.texto}
+                </div>
+                <div className="col-sm-5 px-1">
+                    <input type="text" id={modulo.id} name={modulo.id}/>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/Components/Cuestionario/IES/Modulos/Pregunta_Multiple.js
+++ src/Components/Cuestionario/IES/Modulos/Pregunta_Multiple.js
@@ -1,31 +1,29 @@
 import React from "react";
-import { Col, Form, Row } from "react-bootstrap";
-import PreguntaAbierta from "./Pregunta_Abierta";
+
+const handleChange = e => {
+    // filter response, and send it back to the source through the callback
+}
 
 function PreguntaMultiple({ id, texto, respuestas, callback }) {
-    return (<div>
-        <Form.Group as={Row} className="mb-3">
-            <Form.Label as="legend" column sm={7}>
-                {texto}
-            </Form.Label>
-            <Col sm={5}>
-                {respuestas.map((res, i) => {
-                    return (
-                        <div>
-                            {
-                                (res.modulo === undefined) ? <Form.Check
-                                    type="radio"
-                                    label={res.texto}
-                                    name={id}
-                                    id={res.valor}
-                                /> : <PreguntaAbierta modulo={{texto: res.texto}}/>
-                            }
-                        </div>
-                    );
-                })}
-            </Col>
-        </Form.Group>
-    </div>
+    return (
+        <div className="container border-bottom px-1">
+            <div className="row p-1">
+                <div className="col-sm-12 px-1">
+                    {texto}
+                </div>
+                <div className="col-sm-8 p-1">
+                    {respuestas.map((res, i) => {
+                        return (
+                            <div className="container">
+                                    <input type="radio" id={id} name={id} value={res.valor} />
+                                    <label className="px-2" for={id}>{res.texto}</label>
+                                    {(res.modulo === undefined) ? <></> : <input type="text" className="col-5" />}
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        </div>
     );
 }
 
